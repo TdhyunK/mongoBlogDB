2359bfcc99cb08c69b50607e2fc5549330f3aac5
Thomas Kim <thomaskim@Thomass-MacBook-Pro.local>
2017-11-12 12:32:38 -0500

    commented functions

--- journal_db.py
+++ journal_db.py
@@ -1,11 +1,10 @@
 import pymongo
-# client = pymongo.MongoClient("mongodb://Team08:9vPYcYlOdB8dmFBe@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin")
 
 client = pymongo.MongoClient("mongodb://Team08:9vPYcYlOdB8dmFBe@cluster0-shard-00-00-ppp7l.mongodb.net:27017,cluster0-shard-00-01-ppp7l.mongodb.net:27017,cluster0-shard-00-02-ppp7l.mongodb.net:27017/Team08DB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
-db = client.database_names()
+db = client.Team08DB
 print("db: " + str(db))
 
-# # post blogName userName title postBody tags
+# post blogName userName title postBody tags
 # def post(blog_name, user_name, title, post_body, tags):
 
 
@@ -37,12 +36,15 @@ def user_query():
 		entry_ID = user_query[2]
 		user_name = user_query[3]
 		comment_body = user_query[4]
+		comment(blog_name, entry_ID, user_name, comment_body)
 	elif user_query[0] == "delete" and len(user_query) == 4:
 		blog_name = user_query[1]
 		entry_ID = user_query[2]
 		user_name = user_query[3]
+		delete(blog_name, entry_ID, user_name)
 	elif user_query[0] == "show" and len(user_query) == 2:
 		blog_name = user_query[1]
+		show(blog_name)
 	else:
 		print("Sorry, that is an invalid query. Please try another one.")
 
